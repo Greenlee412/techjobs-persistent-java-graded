a5a46276c4fdf27f629073bb67b1e90f5ac7cce1
Greenlee412 <ryanjgreenlee@gmail.com>
2022-11-09 00:02:40 -0600

    TestTaskThree passing

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,8 +1,12 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -10,6 +14,10 @@ public class Employer extends AbstractEntity {
     @NotNull
     @Size(min = 1, max = 100)
     private String location;
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
 
     public String getLocation() {
         return location;
@@ -19,5 +27,13 @@ public class Employer extends AbstractEntity {
         this.location = location;
     }
 
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
     public Employer(){}
+
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,11 +1,27 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max = 500)
     private String description;
+
+
+    @ManyToMany(mappedBy="skills")
+    private List<Job> jobs = new ArrayList<>();
+
+    //Entity needs an empty constructor remember that
+    public Skill() {}
+
+//    public Skill(String description, List<Job> job) {
+//        this.description = description;
+////        this.jobs = jobs;
 
     public String getDescription() {
         return description;
@@ -15,5 +31,11 @@ public class Skill extends AbstractEntity {
         this.description = description;
     }
 
-    public Skill (){}
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 }
